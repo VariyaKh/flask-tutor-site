e500f9995dc2432852a82b9943d8deafc6b80a77
VariyaKh <variya8@gmail.com>
2020-03-01 21:20:27 +0300

    init

---
+++ tutors_app/utils.py
@@ -0,0 +1,57 @@
+import datetime
+import json
+import os
+
+
+def get_data_json(path):
+    with open(path, "r") as f:
+        tutors = json.load(f)
+    return tutors
+
+
+def get_tutor(path, tutor_id):
+    tutors = get_data_json(path)
+    return [tutor for tutor in tutors if tutor["id"] == tutor_id][0]
+
+
+def get_tutors_by_goal(path, goal):
+    tutors = get_data_json(path)
+    return [tutor for tutor in tutors if goal in tutor["goals"]]
+
+
+def get_hour_and_day():
+    weekdays = dict(zip(range(1, 8), ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]))
+    now_hour = datetime.datetime.now().hour
+    now_day = datetime.datetime.now().weekday()
+    if now_hour % 2:
+        now_hour += 1
+
+    if now_hour == 24:
+        now_hour = 0
+        now_day += 1
+        now_day = now_day % 7 + 1
+
+    if now_hour < 8:
+        now_hour = 8
+    return f"{now_hour}:00", weekdays[now_day]
+
+
+def get_free_tutors(path):
+    tutors = get_data_json(path)
+    now_hour, now_day = get_hour_and_day()
+    print(now_hour, now_day)
+    return [tutor for tutor in tutors if tutor["free"][now_day][now_hour]]
+
+
+def append_json(data_dict, path):
+    if not os.path.isfile(path):
+        with open(path, "w") as f:
+            json.dump([data_dict], f)
+    else:
+        with open(path) as f:
+            data = json.load(f)
+
+        data.append(data_dict)
+
+        with open(path, "w") as f:
+            json.dump(data, f)
---
+++ tutors_app/views.py
@@ -0,0 +1,83 @@
+import random
+
+from flask import Flask, render_template, request
+
+from tutors_app.utils import *
+
+app = Flask(__name__)
+
+tutors_path = "./tutors_app/data/tutors.json"
+weekdays_path = "./tutors_app/data/weekdays.json"
+goals_path = "./tutors_app/data/goals.json"
+booking_path = "./tutors_app/data/booking.json"
+request_path = "./tutors_app/data/request.json"
+
+
+@app.route("/")
+def render_index():
+    tutors = get_free_tutors(tutors_path)
+    return render_template("index.html", tutors=tutors)
+
+
+@app.route("/profiles/")
+def render_profiles():
+    tutors = get_data_json(tutors_path)
+    return render_template("index.html", tutors=tutors)
+
+
+@app.route("/goals/<goal>/")
+def render_goals(goal):
+    goals = get_data_json(goals_path)
+    tutors = get_tutors_by_goal(tutors_path, goal)
+    return render_template("goal.html", goal=goals[goal], tutors=tutors)
+
+
+@app.route("/profiles/<tutor_id>/")
+def render_profile(tutor_id):
+    tutor = get_tutor(tutors_path, int(tutor_id))
+    goals = get_data_json(goals_path)
+    weekdays = get_data_json(weekdays_path)
+    return render_template("profile.html", tutor=tutor, weekdays=weekdays, goals=goals)
+
+
+@app.route("/request/")
+def render_request():
+    return render_template("request.html")
+
+
+@app.route("/request_done/", methods=["GET", "POST"])
+def render_request_done():
+    goals = get_data_json(goals_path)
+    request_form = {
+        "goal": request.form["goal"],
+        "time": request.form["time"],
+        "student": request.form["clientName"],
+        "phone": request.form["clientPhone"],
+    }
+    append_json(request_form, request_path)
+    return render_template("request_done.html", request_form=request_form, goals=goals)
+
+
+@app.route("/booking/<tutor_id>/<day>/<hour>/")
+def render_booking(tutor_id, day, hour):
+    tutor = get_tutor(tutors_path, int(tutor_id))
+    weekdays = get_data_json(weekdays_path)
+    return render_template(
+        "booking.html", tutor=tutor, day=day, hour=hour, weekdays=weekdays
+    )
+
+
+@app.route("/booking_done/", methods=["GET", "POST"])
+def render_booking_done():
+    weekdays = get_data_json(weekdays_path)
+    booking_form = {
+        "day": request.form["clientWeekday"],
+        "hour": request.form["clientTime"],
+        "tutor_id": request.form["clientTeacher"],
+        "student": request.form["clientName"],
+        "phone": request.form["clientPhone"],
+    }
+    append_json(booking_form, booking_path)
+    return render_template(
+        "booking_done.html", booking_form=booking_form, weekdays=weekdays
+    )
